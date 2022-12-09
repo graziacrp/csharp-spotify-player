5f8e2cf94dafd2b2ad7510d1b1d8821773b8bb2a
graziacrp <graziacarpineta25@gmail.com>
2022-12-09 06:45:06 +0100

    Aggiungere i file di progetto.

---
+++ Spotify Player/Brano.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Player
+{
+    internal class Brano : IContenutoRiproducibile
+    {
+        public string nome;
+        public int durata;
+        public string artist;
+        public string album;
+
+            public Brano (string nome, int durata, string artist, string album)
+        {
+            this.nome = nome;
+            this.durata = durata;
+            this.artist = artist;
+            this.album = album;
+        }
+        
+        public void PlayMethod()
+        {
+            Console.WriteLine("Riproduzione del brano Billie Jean di Michael Jackson");
+    }
+    }
+
+  public void PauseMethod()
+    {
+        Console.WriteLine("Messo in pausa il brano Billie Jean di Michael Jackson");
+    }
+
+    public void StopMethod()
+    {
+        Console.WriteLine("Messo in stop il brano Billie Jean di Michael Jackson e riavvolto all'inizio del brano");
+    }
+}
---
+++ Spotify Player/Podcast.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Player
+{
+    internal class Podcast : IContenutoRiproducibile
+    {
+        public string nome;
+        public string autore;
+        public int durata;
+        public Podcast (string nome, string autore, int durata)
+        {
+            this.nome = nome;
+            this.autore = autore;
+            this.durata = durata;
+        }
+       
+        
+        public static void PlayMethod()
+        {
+            Console.WriteLine("Riproduzione del podcast Un uomo di Oriana Fallaci");
+        }
+
+    }
+    public static void StopMethod()
+    {
+        Console.WriteLine("Messo in stop il podcast Un uomo di Oriana Fallaci");
+    }
+
+    public static void PauseMethod()
+    {
+        Console.WriteLine("Messo in pausa il podcast Un uomo di Oriana Fallaci");
+    }
+}
